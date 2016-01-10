731cf1a13556def2d0a5d6da66376847c4dbcde7
Alexandre <alexandre@kimidori>
2016-01-10 03:54:33 +0100

    Corrected merge mistake from last commit

--- Configuration.h
+++ Configuration.h
@@ -9,12 +9,6 @@
 #ifndef _CONFIGURATION_H
 #define _CONFIGURATION_H
 
-<<<<<<< HEAD
-//  PLEASE READ ! ! !
-// 
-//  You need to rename this file into "Configuration.h" in order to compile the project.
-//  Then you must change the values to match you actual configuration.
-
 #define PRIVATE_KEY {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}
 
 #define MAC_ADDRESS {0x01,0x23,0x45,0x67,0x89,0xAB}

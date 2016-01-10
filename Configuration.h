bc03d2290a13940e9e9bb7d3214b762a9f9f52eb
Sebastien Chassot (sinux) <seba.ptl@sinux.net>
2016-01-10 11:42:44 +0100

    Merge devel

--- Configuration.h
+++ Configuration.h
@@ -12,12 +12,13 @@
 #define PRIVATE_KEY {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}
 
 #define MAC_ADDRESS {0x01,0x23,0x45,0x67,0x89,0xAB}
-#define IP_ADDRESS { 192, 168, 137, 39 };
 
-#define SERVER_NAME "192.168.137.204"
+#define IP_ADDRESS { 172, 16, 222, 9 };
+
+#define SERVER_NAME "172.16.222.73"
 
 #define SERVER_PORT 5000
 
-#define API_PATH    "/drinks/api"
+#define API_PATH    "/DrinkingBuddy"
 
 #endif
